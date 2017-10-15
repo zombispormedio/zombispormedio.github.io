418a7f3c7d88c46e02c76891f23ad41122050fdd
Xavier Serrano <zombispormedio007@gmail.com>
2017-10-15 23:27:52 +0200

    Deploy website at Sun Oct 15 2017 23:27:51 GMT+0200 (Hora de verano romance)

---
+++ component---src-pages-contact-js-78f7b4d29678c592c14b.js
@@ -0,0 +1,2 @@
+webpackJsonp([70144966829960],{451:function(e,t,l){(function(a){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var u=l(2);n(u);t.default=function(){return a.createElement("div",null,a.createElement("h1",null,"I'd love to talk! Email me at the address below"),a.createElement("p",null,a.createElement("a",{href:"mailto:me@example.com"},"me@example.com")))},e.exports=t.default}).call(t,l(25))}});
+//# sourceMappingURL=component---src-pages-contact-js-78f7b4d29678c592c14b.js.map
---
+++ path---about-b07d7f6b6e488d52b6dc.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xf927f8900006],{571:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Xavier Serrano - Blog"}}},pathContext:{}}}});
+//# sourceMappingURL=path---about-b07d7f6b6e488d52b6dc.js.map
